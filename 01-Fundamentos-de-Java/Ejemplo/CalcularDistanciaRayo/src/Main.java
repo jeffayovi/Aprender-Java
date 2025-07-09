13a66c124d2a2fd8fa3a192a054041777b46429d
Jefferson Ayovi <105981230+jeffayovi@users.noreply.github.com>
2025-07-09 11:28:14 -0500

    Distancia de rayo

---
+++ 01-Fundamentos-de-Java/Ejemplo/CalcularDistanciaRayo/src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Declaracion de variable
+        double velocidadSonido,intervaloTiempo,distanciaRayo;
+
+        //Inicializar la velocidad del sonido
+        velocidadSonido = 1100.0;
+
+        //Establecer el intervalo de tiempo
+        intervaloTiempo = 7.2;
+
+        //Calcular la distancia al rayo
+        distanciaRayo = velocidadSonido * intervaloTiempo;
+
+        // Visualizar el resultado.
+        System.out.println(" La distancia al rayo es: "+ distanciaRayo + " pies");
+
+
+
+    }
+}
